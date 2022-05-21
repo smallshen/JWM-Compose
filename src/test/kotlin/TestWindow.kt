310ddb492b2e2f4b535d90d11040b4ea73606c1d
xiaoshen <xiaoshen@eridani.club>
2022-05-21 03:02:37 -0500

    hacky code to make IME half working

--- src/test/kotlin/TestWindow.kt
+++ src/test/kotlin/TestWindow.kt
@@ -14,6 +14,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import club.eridani.compose.jwm.ApplicationWindow
 import club.eridani.compose.jwm.ManageWindow
+import club.eridani.compose.jwm.component.textInputFocusable
 import io.github.humbleui.jwm.App
 
 fun main() {
@@ -54,7 +55,8 @@ fun main() {
                             value = title,
                             onValueChange = {
                                 title = it
-                            }
+                            },
+                            modifier = Modifier.textInputFocusable()
                         )
                     }
                 }
